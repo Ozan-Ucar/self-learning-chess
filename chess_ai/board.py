7ae5b479323fdf59c26d946813f57f67795cca1b
ou1534s <ozan-ucar@outlook.de>
2026-03-11 22:08:51 +0100

    implement check detection and legal move filtering

--- chess_ai/board.py
+++ chess_ai/board.py
@@ -86,3 +86,90 @@ class Board:
         
         if move.captured_piece is not None:
             self.pieces[opponent][move.captured_piece] |= (1 << move.to_sq)
+
+    def is_attacked(self, square, by_color):
+        # Checks if a square is attacked by any piece of the given color
+        square_mask = 1 << square
+        occupied = self.get_occupancy()
+        enemy_pieces = self.pieces[by_color]
+        
+        from .move_gen import get_pawn_moves, get_knight_moves, get_king_moves, get_sliding_moves
+        
+        # Check Knights
+        if get_knight_moves(square_mask) & enemy_pieces[Piece.KNIGHT]: return True
+        
+        # Check King
+        if get_king_moves(square_mask) & enemy_pieces[Piece.KING]: return True
+        
+        # Check Pawns (reverse direction as it's an attack TO the square)
+        if by_color == Color.WHITE:
+             attacks = (square_mask >> 7) & ~FILE_A | (square_mask >> 9) & ~FILE_H
+        else:
+             attacks = (square_mask << 7) & ~FILE_H | (square_mask << 9) & ~FILE_A
+        if attacks & enemy_pieces[Piece.PAWN]: return True
+        
+        # Check Sliders (Bishop, Rook, Queen)
+        if get_sliding_moves(square_mask, occupied, 0, (NE, NW, SE, SW)) & \
+           (enemy_pieces[Piece.BISHOP] | enemy_pieces[Piece.QUEEN]): return True
+        if get_sliding_moves(square_mask, occupied, 0, (N, S, E, W)) & \
+           (enemy_pieces[Piece.ROOK] | enemy_pieces[Piece.QUEEN]): return True
+           
+        return False
+
+    def is_in_check(self, color):
+        king_pos = bin(self.pieces[color][Piece.KING]).rfind('1')
+        if king_pos == -1: return False # Should not happen in real chess
+        return self.is_attacked(63 - king_pos, 1 - color)
+
+    def get_legal_moves(self):
+        # We'll need a better piece-to-moves map, but this works for now
+        from .move import Move
+        from .move_gen import get_pawn_moves, get_knight_moves, get_king_moves, get_sliding_moves
+        
+        color = self.turn
+        occupied = self.get_occupancy()
+        own_pieces = self.get_occupancy(color)
+        enemy_pieces_occ = self.get_occupancy(1 - color)
+        
+        legal_moves = []
+        
+        # Iterate over all pieces of current color
+        for piece_type in Piece:
+            pieces = self.pieces[color][piece_type]
+            for i in range(64):
+                if not (pieces & (1 << i)): continue
+                
+                # Get pseudo-legal moves for this piece
+                pseudo_moves = 0
+                if piece_type == Piece.PAWN:
+                    pseudo_moves = get_pawn_moves(1 << i, color, occupied, enemy_pieces_occ)
+                elif piece_type == Piece.KNIGHT:
+                    pseudo_moves = get_knight_moves(1 << i) & ~own_pieces
+                elif piece_type == Piece.KING:
+                    pseudo_moves = get_king_moves(1 << i) & ~own_pieces
+                elif piece_type == Piece.BISHOP:
+                    pseudo_moves = get_sliding_moves(1 << i, occupied, own_pieces, (NE, NW, SE, SW))
+                elif piece_type == Piece.ROOK:
+                    pseudo_moves = get_sliding_moves(1 << i, occupied, own_pieces, (N, S, E, W))
+                elif piece_type == Piece.QUEEN:
+                    pseudo_moves = get_sliding_moves(1 << i, occupied, own_pieces, (N, S, E, W, NE, NW, SE, SW))
+                
+                # Validate moves (must not leave king in check)
+                for j in range(64):
+                    if not (pseudo_moves & (1 << j)): continue
+                    
+                    # Create move object
+                    captured = None
+                    for pt in Piece:
+                        if self.pieces[1-color][pt] & (1 << j):
+                            captured = pt; break
+                    
+                    move = Move(i, j, piece_type, captured_piece=captured)
+                    
+                    # Try it
+                    self.make_move(move)
+                    if not self.is_in_check(color):
+                        legal_moves.append(move)
+                    self.unmake_move(move)
+                    
+        return legal_moves
